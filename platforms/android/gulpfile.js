5407da9f96ffa4d236e1411b0baec8fca91ce739
Ankit Bharat Tanna <ankit.bharat.tanna@accenture.com>
2016-11-10 22:26:38 +0000

    Modularizing Gulp Tasks

--- platforms/android/gulpfile.js
+++ platforms/android/gulpfile.js
@@ -1,3 +1,7 @@
+var build = require('./gulp-tasks/build');
+var serve = require('./gulp-tasks/serve');
+var test = require('./gulp-tasks/test');
+var watch = require('./gulp-tasks/watch');
 var gulp = require('gulp');
 
 gulp.task('watch', watch);
