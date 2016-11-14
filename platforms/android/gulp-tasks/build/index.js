a91598b5c3de2d78a74f39ea2db384aca1ff0a96
Ankit Bharat Tanna <ankit.bharat.tanna@accenture.com>
2016-11-14 21:33:18 +0000

    Completed CSS build process

--- platforms/android/gulp-tasks/build/index.js
+++ platforms/android/gulp-tasks/build/index.js
@@ -2,7 +2,16 @@
 
 module.exports = function (gulp) {
 
+	var rimraf = require('gulp-rimraf');
+
     gulp.task('build-js', function () {
 
+    });
+
+    gulp.task('build-css', ['css-prefix', 'css-lint', 'css-concat']);
+
+    gulp.task('clean', function() {
+        return gulp.src(['dist/*'])
+            .pipe(rimraf());
     });
 };
--- platforms/android/gulp-tasks/quality/index.js
+++ platforms/android/gulp-tasks/quality/index.js
@@ -6,6 +6,13 @@ var htmlhint = require('gulp-htmlhint');
 var eslint = require('gulp-eslint');
 var eslintConfig = JSON.parse(fs.readFileSync('./.eslintrc.json'));
 var reporter = require('eslint-html-reporter');
+var csslint = require('gulp-csslint');
+var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+var concat = require('gulp-concat');
+var cleanCSS = require('gulp-clean-css');
+
+csslint.addFormatter('csslint-stylish');
 
 module.exports = function(gulp) {
 	gulp.task('lint-html', function() {
@@ -19,31 +26,56 @@ module.exports = function(gulp) {
 	});
 
 	gulp.task('es-lint', function() {
-            return gulp.src(['./assets/www/**/*.js',
-                             '!node_modules/**',
-                             '!./assets/www/cordova-js-src/**',
-                             '!./assets/www/cordova*.js',
-                             '!./assets/www/lib/**',
-                             '!./assets/www/plugins/**',
-                             '!./assets/www/spec/lib/**'
-                             ])
-                .pipe(eslint(
-                    {
-						rules: eslintConfig,
-						globals: ['jQuery', '$', 'angular'],
-                        envs: ['browser']
-                    }
-                ))
-                .pipe(eslint.formatEach('compact', process.stderr))
-                .pipe(eslint.result(function(result) {
-                      console.log('ESLint result: ' + result.filePath);
-                      console.log('# Messages: ' + result.messages.length);
-                      console.log('# Warnings: ' + result.warningCount);
-                      console.log('# Errors: ' + result.errorCount);
-                 }))
-                .pipe(eslint.format(reporter, function(results) {
-                            fs.writeFileSync(path.join(__dirname, 'report-results.html'), results);
-                          }))
-                .pipe(eslint.failAfterError());
+        return gulp.src(['./assets/www/**/*.js',
+                         '!node_modules/**',
+                         '!./assets/www/cordova-js-src/**',
+                         '!./assets/www/cordova*.js',
+                         '!./assets/www/lib/**',
+                         '!./assets/www/plugins/**',
+                         '!./assets/www/spec/lib/**'
+                         ])
+        .pipe(eslint(
+            {
+				rules: eslintConfig,
+				globals: ['jQuery', '$', 'angular'],
+                envs: ['browser']
+            }
+        ))
+        .pipe(eslint.formatEach('compact', process.stderr))
+        .pipe(eslint.result(function(result) {
+              console.log('ESLint result: ' + result.filePath);
+              console.log('# Messages: ' + result.messages.length);
+              console.log('# Warnings: ' + result.warningCount);
+              console.log('# Errors: ' + result.errorCount);
+         }))
+        .pipe(eslint.format(reporter, function(results) {
+                    fs.writeFileSync(path.join(__dirname, 'report-results.html'), results);
+                  }))
+        .pipe(eslint.failAfterError());
 	});
+
+	gulp.task('css-prefix', function() {
+		gulp.src(['./assets/www/css/*.css'])
+            .pipe(autoprefixer({
+                browsers: ['last 5 versions'],
+                cascade: false
+            }))
+            .pipe(gulp.dest('./assets/www/css'));
+	});
+
+	gulp.task('css-lint', function() {
+      gulp.src(['./assets/www/css/*.css', '!./assets/www/css/font-awesome.min.css'])
+        .pipe(csslint())
+        .pipe(csslint.formatter('stylish'))
+        .pipe(csslint.formatter('fail'));
+    });
+
+    gulp.task('css-concat', function() {
+        gulp.src(['./assets/www/css/*.css'])
+            .pipe(sourcemaps.init())
+            .pipe(concat('main.css'))
+            .pipe(cleanCSS({compatibility: 'ie8'}))
+            .pipe(sourcemaps.write('.'))
+            .pipe(gulp.dest('dist/css'));
+    });
 };
